a0ad3ed8290c5d9adf2924d905a9a26d12b5768c
OrionNavattan <103385399+OrionNavattan@users.noreply.github.com>
2022-11-05 19:50:21 -0700

    Minor change to assemblyinfo

--- Dual PCM Compress/Properties/AssemblyInfo.cs
+++ Dual PCM Compress/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Dual PCM Compress")]
+[assembly: AssemblyTitle("S2 SoundDriver Compress")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Dual PCM Compress")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyProduct("S2 SoundDriver Compress")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
