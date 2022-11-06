085d5057a0cdb4536ed3f1bbe1fa2229c03155a0
OrionNavattan <103385399+OrionNavattan@users.noreply.github.com>
2022-11-05 19:44:40 -0700

    Successfully modified to compress Sonic 2's sound driver. :>

--- Dual PCM Compress/Program.cs
+++ Dual PCM Compress/Program.cs
@@ -12,16 +12,16 @@ namespace Dual_PCM_Compress {
 		static void Main(string[] args) {
 			if (args.Length < 4) {
 				Console.Write(
-					"Usage: \"Dual PCM Compress\" <driver bin> <settings> <output file> <compressor>\n" +
-					"<driver bin> :  Binary output data for the Dual PCM driver\n" +
+					"Usage: \"S2 Sound Driver Compress\" <driver bin> <settings> <output file> <compressor> <-a>\n" + 
+					"<driver bin> :  Binary output data for the Sonic 2 driver\n" +
 					"<settings> :    Settings file containing the data needed to fix the driver binary\n" +
 					"<output file> : File to put the compressed data at\n" +
-					"<compressor> :  The file location of koscmp.exe\n\n" +
-					"This tool was mainly created to help with compressing Dual PCM ion a way that\n" +
-					"doesn't suddenly break for no reason. This provides a more robust method for\n" +
-					"correcting the driver binary and compressing the driver to Kosinski. Other\n" +
-					"compressors could be used, so long as they expect the first argument to be the\n" +
-					"input file and second the output file."
+					"<compressor> :  The file location of the saxman compressor\n" +
+					"<-a> : Optional flag for accurate sound driver compression \n" + 
+					"This is a modified version of Natsumi's Dual PCM Compress, tailored to the needs\n" +
+					"of Sonic the Hedgehog 2. It has been modified to correctly pass the -a argument for\n" +
+					"accurate compression, and to allow it to patch the Saxman decompressor in the ROM\n" +
+					"with the size of the compressed driver binary.\n" 
 				);
 
 				Console.ReadKey();
@@ -37,7 +37,7 @@ namespace Dual_PCM_Compress {
 				}
 
 				if (!File.Exists(args[1])) {
-					Console.WriteLine("Unable to read input file " + args[1]);
+					Console.WriteLine("Unable to read input file " + args[1]); // settings will contain an additional entry: the location of the sound driver size in the Saxman decompressor
 					Console.ReadKey();
 					return;
 				}
@@ -50,8 +50,9 @@ namespace Dual_PCM_Compress {
 
 				// load settings and determine the addresses
 				byte[] settings = File.ReadAllBytes(args[1]);
-				int outaddr = ReadLong(settings, 0);
+				int outaddr = ReadLong(settings, 0); 
 				int maxlen = ReadLong(settings, 4);
+			 	int patchaddr = ReadLong(settings, 8); // new variable containing the location of the value to patch in the Saxman decompressor
 
 				long inlen = new FileInfo(args[2]).Length;
 
@@ -64,8 +65,8 @@ namespace Dual_PCM_Compress {
 				{ // fix the file
 					byte[] data = File.ReadAllBytes(args[0]);
 
-					for (int i = 8;i < settings.Length;) {
-						data[(settings[i++] << 8) | settings[i++]] = settings[i++];
+					for (int i = 12;i < settings.Length;) {
+						data[(settings[i++] << 12) | settings[i++]] = settings[i++];
 
 						if (settings[i++] != '>') {
 							Console.WriteLine($"Unexpected delimiter {settings[i - 1]}");
@@ -78,7 +79,7 @@ namespace Dual_PCM_Compress {
 				}
 
 				{ // compress the file
-					Process comp = Process.Start(new ProcessStartInfo(args[3], $"\"{args[0]}\" \"{args[0]}.kos\"") {
+					Process comp = Process.Start(new ProcessStartInfo(args[3], $"\"{args[4]}\" \"{args[0]}\" \"{args[0]}.sax\" ") { // output as .sax instead
 						WorkingDirectory = Directory.GetCurrentDirectory(),
 						UseShellExecute = false,
 						CreateNoWindow = true,
@@ -90,41 +91,57 @@ namespace Dual_PCM_Compress {
 					if (!comp.HasExited) comp.Kill();
 
 					// check we succeeded
-					if (!File.Exists(args[0] + ".kos")) {
+					if (!File.Exists(args[0] + ".sax")) {
 						Console.WriteLine("Could not compress file correctly, unable to continue.");
 						Console.ReadKey();
 						return;
 					}
 				}
+				// get length of compressed driver
+				long actuallen = new FileInfo(args[0] + ".sax").Length;
+				ushort sizepatchinter = Convert.ToUInt16(actuallen);
 
-				long actuallen = new FileInfo(args[0] + ".kos").Length;
-
+				// if it is 0 or less, it probably failed
 				if (actuallen <= 0) {
 					Console.WriteLine("Could not compress file correctly, unable to continue.");
 					Console.ReadKey();
 					return;
 				}
 
-				// check if it fits
+				// check if the compressed driver fits
 				if(actuallen > maxlen) {
 					Console.WriteLine($"Compressed sound driver does not fit! Increase Z80_Space to ${actuallen.ToString("X")} and build again.");
 					Console.ReadKey();
 					return;
 				}
 
+				// convert actuallen to big-endian word for patching saxman compressor
+		
+				byte[] sizepatch = BitConverter.GetBytes(sizepatchinter);
+    			Array.Reverse(sizepatch);
+
 				// copy the compressed file data
-				using(FileStream fs = File.OpenWrite(args[2])) {
-					fs.Seek(outaddr, SeekOrigin.Begin);
+				using(FileStream fs = File.OpenWrite(args[2])) { // open rom file
+					fs.Seek(outaddr, SeekOrigin.Begin);			// address where compressed driver will be written
 
-					using (FileStream cf = File.OpenRead(args[0] + ".kos")) {
-						cf.CopyTo(fs, (int)actuallen);
+					using (FileStream cf = File.OpenRead(args[0] + ".sax")) { // open compressed driver, change to .sax
+						cf.CopyTo(fs, (int)actuallen); // write compressed driver
+					
 					}
-
-					fs.Flush();
+				
 				}
 
+				using(FileStream fs = File.OpenWrite(args[2])) { // open rom file
+
+					fs.Seek(patchaddr, SeekOrigin.Begin); // address where size will be patched in Saxman decompressor
+					
+					using(BinaryWriter w = new BinaryWriter(fs)) {
+						w.Write(sizepatch);
+					}	
+
+				}
 				// delete files and send message
-				File.Delete(args[0] + ".kos");
+				File.Delete(args[0] + ".sax");
 				File.Delete(args[0]);
 				File.Delete(args[1]);
 				Console.WriteLine($"Success! Compressed driver size is ${actuallen.ToString("X")}!");
